885008a9e489b145543f33222c794f45b62380dc
Sarthak-04 <4sarthakagrawal@gmail.com>
2017-03-01 19:26:30 +0530

    donation cart template without tooltip

---
+++ js/script-main.js
@@ -0,0 +1,18 @@
+$(function () { 
+	$('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
+
+});  
+
+	var donationBox = document.getElementById('donation-box'),
+	donationButton = document.getElementById('donation-btn');
+	donationButton.onclick = function() {
+		var progressBar = $('.progress-bar'),
+		progressBarCurrentVal = progressBar.attr('aria-valueNow'),
+		inputValue = donationBox.value;
+		// by 10 to convert to width percentage
+		progressBarCurrentVal = (+inputValue) + +progressBarCurrentVal;
+		var width = progressBarCurrentVal/10;
+		progressBar.css('width', width+'%').attr('aria-valuenow', progressBarCurrentVal);
+	}
+
+//donationButton.onClick = calculateFundValue();
