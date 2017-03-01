f1c5981795d67db7a5069e204fa9bc186b84ab4d
Sarthak-04 <4sarthakagrawal@gmail.com>
2017-03-01 20:49:53 +0530

    added tooltip

--- js/script-main.js
+++ js/script-main.js
@@ -1,7 +1,10 @@
 $(function () { 
-
+	$('progress').tooltip('show'); 
 });  
-
+/*
+$( document ).ready(function() {
+    
+});*/
 	var donationBox = document.getElementById('donation-box'),
 	donationButton = document.getElementById('donation-btn');
 	donationButton.onclick = function() {
@@ -10,8 +13,10 @@ $(function () {
 		inputValue = donationBox.value;
 		// by 10 to convert to width percentage
 		progressBarCurrentVal = (+inputValue) + +progressBarCurrentVal;
-		var width = progressBarCurrentVal/10;
+		var width = progressBarCurrentVal/10,
+		amountRemaining = 1000 - progressBarCurrentVal;
 		progressBar.css('width', width+'%').attr('aria-valuenow', progressBarCurrentVal);
+		$('.progress').attr('title',"$" + amountRemaining + " still needed for this project");
 	}
 
 //donationButton.onClick = calculateFundValue();
