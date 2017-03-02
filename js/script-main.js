5dbfcaed83929dedb9d3c3907e06000b05aaf2a7
Sarthak-04 <4sarthakagrawal@gmail.com>
2017-03-02 08:32:40 +0530

    added top tooltip component

--- js/script-main.js
+++ js/script-main.js
@@ -1,10 +1,7 @@
 $(function () { 
-	$('progress').tooltip('show'); 
+	$('progress').tooltip();
 });  
-/*
-$( document ).ready(function() {
-    
-});*/
+	
 	var donationBox = document.getElementById('donation-box'),
 	donationButton = document.getElementById('donation-btn');
 	donationButton.onclick = function() {
@@ -16,7 +13,33 @@ $( document ).ready(function() {
 		var width = progressBarCurrentVal/10,
 		amountRemaining = 1000 - progressBarCurrentVal;
 		progressBar.css('width', width+'%').attr('aria-valuenow', progressBarCurrentVal);
-		$('.progress').attr('title',"$" + amountRemaining + " still needed for this project");
+		//$('.progress').attr('title',"$" + amountRemaining + " still needed for this project");
+		$('#donation-left').text("$" + amountRemaining + " still needed for this project")
 	}
 
-//donationButton.onClick = calculateFundValue();
+/*	window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '421240411555274',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+    //FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s);
+     js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+	document.getElementById('btn-two').onclick = function() {
+	FB.ui({
+	method: 'share',
+	display: 'popup',
+	href: 'https://developers.facebook.com/docs/',
+	quote: 'Yay, I donated!'
+	}, function(response){})};
+ */
