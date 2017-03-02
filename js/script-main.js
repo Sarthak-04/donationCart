f89b064902e9f18a44d7fcebb32126287d72b2fd
Sarthak-04 <4sarthakagrawal@gmail.com>
2017-03-02 20:30:52 +0530

    added all functionality

--- js/script-main.js
+++ js/script-main.js
@@ -5,16 +5,32 @@ $(function () {
 	var donationBox = document.getElementById('donation-box'),
 	donationButton = document.getElementById('donation-btn');
 	donationButton.onclick = function() {
+		$('.error-message').css('display','none');
 		var progressBar = $('.progress-bar'),
 		progressBarCurrentVal = progressBar.attr('aria-valueNow'),
 		inputValue = donationBox.value;
-		// by 10 to convert to width percentage
-		progressBarCurrentVal = (+inputValue) + +progressBarCurrentVal;
-		var width = progressBarCurrentVal/10,
-		amountRemaining = 1000 - progressBarCurrentVal;
-		progressBar.css('width', width+'%').attr('aria-valuenow', progressBarCurrentVal);
-		//$('.progress').attr('title',"$" + amountRemaining + " still needed for this project");
-		$('#donation-left').text("$" + amountRemaining + " still needed for this project")
+		// only calculate if input vallue is a positive integer
+		if(inputValue.match(/^[0-9]*[1-9][0-9]*$/)) {
+			// by 10 to convert to width percentage
+			progressBarCurrentVal = (+inputValue) + +progressBarCurrentVal;
+			var width = progressBarCurrentVal/10,
+			amountRemaining = 1000 - progressBarCurrentVal;
+			// clear input on submit only when value is correct;
+			donationBox.value="";
+			if (amountRemaining <= 0) {
+				progressBarCurrentVal = 1000;
+				$('#donation-left').text("Thank you! Target met.");
+				progressBar.css('width', 100+'%').attr('aria-valuenow', 1000);
+				progressBar.css('background-color', '#5cb85c');
+			} 
+			else {
+				progressBar.css('width', width+'%').attr('aria-valuenow', progressBarCurrentVal);
+				$('#donation-left').text("$" + amountRemaining + " still needed for this project");
+			}
+		}
+		else {
+			$('.error-message').css('display','block');
+		}	
 	}
 
 	window.fbAsyncInit = function() {
